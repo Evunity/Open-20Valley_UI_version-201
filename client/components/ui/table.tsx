21b47869283e9362c010d94e59cbdee755b4f2cb
Mohammed Haitham <113844625+Mohammed20037@users.noreply.github.com>
2026-03-28 15:25:50 +0300

    Fix workflow edge anchoring to exact handle centers

--- client/components/ui/table.tsx
+++ client/components/ui/table.tsx
@@ -9,7 +9,7 @@ const Table = React.forwardRef<
   <div className="relative w-full overflow-auto">
     <table
       ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
+      className={cn("w-full caption-bottom text-sm border-collapse", className)}
       {...props}
     />
   </div>
@@ -73,7 +73,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-10 sm:h-12 px-2 sm:px-4 text-left align-middle typo-label [&:has([role=checkbox])]:pr-0",
+      "h-9 sm:h-10 px-3 sm:px-4 text-left align-middle typo-label [&:has([role=checkbox])]:pr-0",
       className,
     )}
     {...props}
@@ -87,7 +87,7 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-2 sm:p-4 align-middle text-sm [&:has([role=checkbox])]:pr-0", className)}
+    className={cn("px-3 py-2 sm:px-4 sm:py-2.5 align-middle text-sm [&:has([role=checkbox])]:pr-0", className)}
     {...props}
   />
 ));
